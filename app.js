db9e1df07f3779d2a870cb5fc25cff3159b16db5
razaq himawan <razaqhim@gmail.com>
2025-09-07 17:08:46 +0700

    feat: add dockerfile

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const passportCfg = require("./config/passport");
 const passport = require("passport");
 const routesCfg = require("./config/routes");
 
+const Envs = require("./config/env");
+
 const app = express();
 
 expressCfg(app, express, passport);
 passportCfg(passport);
 routesCfg(app);
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Example app listening on port http://localhost:${process.env.PORT}`
-  );
+app.listen(Envs.PORT, () => {
+  console.log(`Example app listening on port http://localhost:${Envs.PORT}`);
 });
